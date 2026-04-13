586ee0ade4fcac45a6a95629d48ddd4311672ba8
Francois-Guillaume Ribreau <github@fgribreau.com>
2026-04-13 05:20:45 +0000

    update

--- vendor/composer/installed.php
+++ vendor/composer/installed.php
@@ -3,7 +3,7 @@
         'name' => 'image-charts/image-charts',
         'pretty_version' => 'dev-master',
         'version' => 'dev-master',
-        'reference' => '51661259f44746b8ef2137b1eddf94bea1c804db',
+        'reference' => 'c8f1c0b498c4d6af4064372ab540de9b364e3748',
         'type' => 'library',
         'install_path' => __DIR__ . '/../../',
         'aliases' => array(),
@@ -13,7 +13,7 @@
         'image-charts/image-charts' => array(
             'pretty_version' => 'dev-master',
             'version' => 'dev-master',
-            'reference' => '51661259f44746b8ef2137b1eddf94bea1c804db',
+            'reference' => 'c8f1c0b498c4d6af4064372ab540de9b364e3748',
             'type' => 'library',
             'install_path' => __DIR__ . '/../../',
             'aliases' => array(),
